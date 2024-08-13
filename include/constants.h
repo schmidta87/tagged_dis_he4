199161b9b24eb2f1faaf5cb6c7199ae8b062de7a
Axel Schmidt <axelschmidt@gwu.edu>
2024-08-13 09:54:44 -0400

    Added constants.h include file.

---
+++ include/constants.h
@@ -0,0 +1,20 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+
+// Masses of various nuclei and particles
+const double mU = 0.9315;
+const double me = 0.000510998;
+const double mP = 0.938272;
+const double mN = 0.939565;
+const double mH2 = 2.014102*mU-me;
+const double mH3 = 3.0160492*mU-me;
+const double mHe3 = 3.0160293*mU - 2.*me;
+const double mHe4 = 4.0026033*mU - 2.*me;
+
+
+// Constants of nature
+const double alpha_em = 0.0072973525643;
+
+
+#endif
